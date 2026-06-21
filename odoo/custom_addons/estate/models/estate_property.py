91cba77e59933a52c5ca85f966293bd04d0ab3d3
MOHAMED . <mohamed.walid.4836@gmail.com>
2026-06-21 09:15:59 +0300

    first commit

---
+++ odoo/custom_addons/estate/models/estate_property.py
@@ -0,0 +1,26 @@
+from odoo import models, fields
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+
+    date_availability = fields.Date(default=lambda self: date.today() + relativedelta(months=3), copy=False)
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        help="Orientation of the garden"
+    )
